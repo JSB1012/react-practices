527b34acb9b2f46aa5b101cf2ac2f2516b4d082e
JSB1012 <sbj15102@naver.com>
2022-07-15 16:40:20 +0900

    commit

--- 1.basics/ex08/src/08/Clock.js
+++ 1.basics/ex08/src/08/Clock.js
@@ -1,6 +1,6 @@
     import React from 'react';
 
-    function Clock(props) {
+    function Clock() {
         const date = new Date();
         const hours = date.getHours();
         const minutes = date.getMinutes();
@@ -10,22 +10,21 @@
             /*
                 comment01: JSX 밖은 JavaScript 구문(주석 구문)이 가능하다.
             */
-            <div 
+            <div
                 /*
-                    comment02: 여기서도 JavaScript 주석이 가능하다. 비추
+                    comment02: 여기서도 JavaScript 주석이 가능하다: 비추
                 */
                 className='clock'
                 title='시계'>
-                {
-                    /*
-                        comment03: 이런 방식으로 표현식이 실행되는 블록안에서 주석을 달아야 한다. 강추
-
-                    */
-                }
+                {/*
+                    comment03: 이런 방식으로 표현식이 실행되는 블록안에서 주석을 달아야 한다: 강추
+                        
+                */}    
                 {('0' + (hours > 12 ? hours - 12 : hours)).slice(-2)}
                 {' : '}
                 {/*
-                    comment03: JSX는 HTML이 아니다!!!
+                    comment03: JSX는 HTML이 아니다!!!!!
+                    <!--   --> HTML 주석은 사용할 수 없다.
                 */}
                 {('0' + minutes).slice(-2)}
                 {' : '} 
